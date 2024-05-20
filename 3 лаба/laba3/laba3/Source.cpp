ab1fbdfe6dfe4f7cf1f927331f6d2a441c4f2f8e
Serg <aaa@gmail.com>
2024-05-20 14:25:35 +0300

    add2

--- 3 лаба/laba3/laba3/Source.cpp
+++ 3 лаба/laba3/laba3/Source.cpp
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, res;
+	int all, b, c, d, res;
 	scanf_s("%d%d%d%d", &a, &b, &c, &d);
-	res = int((a > 0)) + int((b > 0)) + int((c > 0)) + int((d > 0));
+	res = int((all > 0)) + int((b > 0)) + int((c > 0)) + int((d > 0));
 	if (res == 4)
 	{
-		printf("%d %d %d\n", a, b, c);
+		printf("%d %d %d\n", all, b, c);
 		printf("%d %d %d\n", b, c, d);
-		printf("%d %d %d\n", a, c, d);
+		printf("%d %d %d\n", all, c, d);
 	}
 	else {
 		if (res == 3)
 		{
-			if (a < 0)
+			if (all < 0)
 			{
 				printf("%d %d %d\n", b, c, d);
 			}
 			if (b < 0)
 			{
-				printf("%d %d %d\n", a, c, d);
+				printf("%d %d %d\n", all, c, d);
 			}
 			if (c < 0)
 			{
-				printf("%d %d %d\n", a, b, d);
+				printf("%d %d %d\n", all, b, d);
 			}
 			if (d < 0)
 			{
-				printf("%d %d %d\n", a, b, c);
+				printf("%d %d %d\n", all, b, c);
 			}
 		}
 		else 
