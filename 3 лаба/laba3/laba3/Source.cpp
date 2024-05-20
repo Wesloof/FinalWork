02ecae624bea66d8338541c8f6bec63437396fa4
paxom <kladowkd@yandex.ru>
2024-05-20 14:08:15 +0300

    pedorin1

--- 3 лаба/laba3/laba3/Source.cpp
+++ 3 лаба/laba3/laba3/Source.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, res;
-	scanf_s("%d%d%d%d", &a, &b, &c, &d);
-	res = int((a > 0)) + int((b > 0)) + int((c > 0)) + int((d > 0));
+	int al, b, c, d, res; // переменные
+	scanf_s("%d%d%d%d", &al, &b, &c, &d);
+	res = int((al > 0)) + int((b > 0)) + int((c > 0)) + int((d > 0));
 	if (res == 4)
 	{
-		printf("%d %d %d\n", a, b, c);
+		printf("%d %d %d\n", al, b, c);
 		printf("%d %d %d\n", b, c, d);
-		printf("%d %d %d\n", a, c, d);
+		printf("%d %d %d\n", al, c, d);
 	}
 	else {
 		if (res == 3)
@@ -19,15 +19,15 @@ int main() {
 			}
 			if (b < 0)
 			{
-				printf("%d %d %d\n", a, c, d);
+				printf("%d %d %d\n", al, c, d);
 			}
 			if (c < 0)
 			{
-				printf("%d %d %d\n", a, b, d);
+				printf("%d %d %d\n", al, b, d);
 			}
 			if (d < 0)
 			{
-				printf("%d %d %d\n", a, b, c);
+				printf("%d %d %d\n", al, b, c);
 			}
 		}
 		else 
